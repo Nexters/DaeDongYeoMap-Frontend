57ebe6b39d11cac4ab318f303dcbbb5d5300ec99
dev-owen <jgyuity1289@gmail.com>
2021-01-09 01:39:37 +0900

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>데동여지도</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          데동여지도<br />DaeDongYeoMap
+        </h1>
+
+        <p className={styles.description}>
+          Team Oh! When?
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
