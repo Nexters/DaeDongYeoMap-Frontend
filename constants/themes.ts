ec4fc0b29fa2142d9ee7d0f1429d2f22a5bbd066
load0ne <jjazan201@gmail.com>
2021-01-15 04:40:31 +0900

    (#2) 스팟찍기레이어 이모지 선택페이지 컴포넌트 추가

---
+++ constants/themes.ts
@@ -0,0 +1,15 @@
+// TODO: 추후 BE에서 관리예정
+
+export interface Theme {
+  color: string;
+  label: string;
+}
+
+const themes: Theme[] = [
+  { color: '#ff7474', label: '로맨틱한' },
+  { color: '#7a65ff', label: '신나는' },
+  { color: '#35d6b9', label: '시크한' },
+  { color: '#3a87fc', label: '자유로운' },
+];
+
+export default themes;
