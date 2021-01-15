0db5e1bbf79bf4559a719a5b73379e450792dcb2
dev-owen <jgyuity1289@gmail.com>
2021-01-16 01:22:33 +0900

    feat: 지정된 위치에 중첩된 원 두 개 그리기

--- components/Home/MainMapPage/MapArea/MapArea.tsx
+++ components/Home/MainMapPage/MapArea/MapArea.tsx
@@ -20,14 +20,52 @@ const MapArea: React.FC = () => {
         const el = document.getElementById('map');
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const map = new (window as any).kakao.maps.Map(el, {
-          center: new (window as any).kakao.maps.LatLng(33.450701, 126.570667),
+          center: new (window as any).kakao.maps.LatLng(
+            33.2588962209144,
+            126.40716457908
+          ),
           level: 3,
         });
+
+        const circle = new (window as any).kakao.maps.Circle({
+          center: new (window as any).kakao.maps.LatLng(
+            33.2588962209144,
+            126.40716457908
+          ), // 원의 중심좌표 입니다
+          radius: 50,
+          strokeWeight: 1, // 선의 두께입니다
+          strokeColor: '#73E2A0', // 선의 색깔입니다
+          strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle: 'solid',
+          fillColor: '#73E2A0',
+          fillOpacity: 0.3,
+        });
+
+        const circle2 = new (window as any).kakao.maps.Circle({
+          center: new (window as any).kakao.maps.LatLng(
+            33.2588962209144,
+            126.40716457908
+          ), // 원의 중심좌표 입니다
+          radius: 100,
+          strokeWeight: 1, // 선의 두께입니다
+          strokeColor: '#73E2A0', // 선의 색깔입니다
+          strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+          strokeStyle: 'solid',
+          fillColor: '#73E2A0',
+          fillOpacity: 0.12,
+        });
+
+        circle.setMap(map);
+        circle2.setMap(map);
       });
+    };
+
+    return () => {
+      document.head.removeChild(script);
     };
   }, []);
 
-  return <$.MapArea></$.MapArea>;
+  return <$.MapArea />;
 };
 
 export default MapArea;
