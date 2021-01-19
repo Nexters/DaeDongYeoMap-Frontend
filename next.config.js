e59db9eed1e67940aca95322149b209635f6b408
load0ne <jjazan201@gmail.com>
2021-01-20 02:01:57 +0900

    feat: webpack alias 설정 및 불필요한 파일 제거

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const Path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   webpack: (config) => {
     config.plugins.push(new Dotenv({ silent: true }));
+    config.resolve.alias['~'] = Path.join(process.cwd(), '.');
+
     return config;
   },
 };
