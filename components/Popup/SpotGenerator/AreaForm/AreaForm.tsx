3d75f5de4a32f04dae9b34e2035d65ee87297bd6
load0ne <jjazan201@gmail.com>
2021-01-28 01:54:19 +0900

    feat: 스팟찍기팝업 폼요소 마크업

--- components/Popup/SpotGenerator/AreaForm/AreaForm.tsx
+++ components/Popup/SpotGenerator/AreaForm/AreaForm.tsx
@@ -29,6 +29,19 @@ const AreaForm: React.FC = () => {
 
   return (
     <$.AreaForm>
+      <$.FieldSet>
+        <$.FieldLabel>누구와 함께 방문하셨나요</$.FieldLabel>
+        <$.FieldInputBox>
+          <$.InputVisitant placeholder="ex) 전남친, 사랑하는 엄마" />
+        </$.FieldInputBox>
+      </$.FieldSet>
+      <$.FieldSet>
+        <$.FieldLabel>장소를 방문한 날짜를 기록해보세요</$.FieldLabel>
+        <$.FieldInputBox>
+          <$.DateLabel isSelected={false}>01.16.2021</$.DateLabel>
+          <$.DatePickerOpenButton />
+        </$.FieldInputBox>
+      </$.FieldSet>
       <$.SubmitButton>완료하기</$.SubmitButton>
     </$.AreaForm>
   );
--- components/Popup/SpotGenerator/AreaForm/AreaFormView.tsx
+++ components/Popup/SpotGenerator/AreaForm/AreaFormView.tsx
@@ -5,11 +5,68 @@ import painter from '~/styles/theme/painter';
 export const AreaForm = styled.div`
   flex-shrink: 0;
   flex-grow: 0;
+  margin-top: 49px;
+`;
+
+export const FieldSet = styled.div`
+  & + & {
+    margin-top: 42px;
+  }
+`;
+
+export const FieldLabel = styled.strong`
+  display: block;
+  font-size: 20px;
+  color: ${painter.grayscale['2']};
+`;
+
+export const FieldInputBox = styled.div`
+  position: relative;
+  height: 56px;
+  margin-top: 16px;
+  border: 1px solid rgba(0, 0, 0, 0.08);
+  border-radius: 8px;
+  background-color: #fff;
+`;
+
+export const InputVisitant = styled.input.attrs({
+  type: 'text',
+})`
+  display: block;
+  width: 100%;
+  height: 100%;
+  padding-left: 16px;
+  font-weight: 600;
+  font-size: 18px;
+  color: ${painter.basic.black};
+  ${painter.form.placeholder('#cdcdcd')}
+`;
+
+export const DateLabel = styled.strong<{
+  isSelected: boolean;
+}>`
+  display: block;
+  height: 100%;
+  padding-left: 16px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 54px;
+  color: ${(props) =>
+    props.isSelected ? painter.basic.black(props) : '#cdcdcd'};
+`;
+
+export const DatePickerOpenButton = styled.a.attrs({ href: '#' })`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `;
 
 export const SubmitButton = styled.a.attrs({ href: '#', role: 'button' })`
   display: block;
   height: 64px;
+  margin-top: 41px;
   border-radius: 12px;
   background-color: #101721;
   font-weight: bold;
