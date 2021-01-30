73df9e6c4f9c6e53a01f8922a121b135fc3f08e9
load0ne <jjazan201@gmail.com>
2021-01-30 18:38:47 +0900

    feat: 캘린더 상태연산 모듈 추가

---
+++ util/Calendar/Calendar.ts
@@ -0,0 +1,114 @@
+import CalendarDate, { Day } from './CalendarDate';
+import type { UnixTime } from './CalendarDate';
+
+export type CalendarOption = {
+  limit?: {
+    first?: Date;
+    last?: Date;
+  };
+};
+
+export type CalendarMatrix = Array<Array<CalendarDate>>;
+
+const UNIX_TIME = {
+  ONE_DAY: 24 * 60 * 60,
+};
+
+export default class Calendar {
+  // 현재 날짜
+  private cursor: Date;
+  // 최대, 최소 날짜
+  private limit: {
+    first: UnixTime;
+    last: UnixTime;
+  };
+
+  constructor(cursor: Date, option: CalendarOption = {}) {
+    this.initLimitOption(cursor, option);
+    this.cursor = cursor;
+  }
+
+  public getMatrix(webDate?: Date): CalendarMatrix {
+    const target = webDate || this.cursor;
+    const dateOption = { cursor: this.cursor, limit: this.limit };
+
+    const matrix: CalendarMatrix = [[]];
+
+    // 이전달 날짜 추가
+    {
+      const cursor: CalendarDate = CalendarDate.createLastDateOfPrevMonth(
+        target,
+        dateOption
+      );
+
+      while (cursor.getDay() !== Day.Saturday) {
+        matrix[matrix.length - 1].unshift(cursor.clone());
+        cursor.mutateToPrevDate();
+      }
+    }
+
+    // 이번달 날짜 추가
+    {
+      const cursor: CalendarDate = CalendarDate.createFirstDate(
+        target,
+        dateOption
+      );
+      const lastDate: number = CalendarDate.createLastDate(
+        target,
+        dateOption
+      ).getDate();
+      let cursorDate: number = cursor.getDate();
+
+      while (cursorDate <= lastDate) {
+        matrix[matrix.length - 1].push(cursor.clone());
+        cursor.mutateToNextDate();
+        cursorDate = cursor.getDate();
+
+        if (matrix[matrix.length - 1].length > 6) {
+          matrix.push([]);
+        }
+      }
+    }
+
+    // 다음달 날짜 추가
+    {
+      const cursor: CalendarDate = CalendarDate.createFirstDateOfNextMonth(
+        target,
+        dateOption
+      );
+
+      while (cursor.getDay() !== Day.Sunday) {
+        matrix[matrix.length - 1].push(cursor.clone());
+        cursor.mutateToNextDate();
+      }
+    }
+
+    // 마지막 빈배열이 추가되었다면 삭제
+    if (matrix[matrix.length - 1].length === 0) {
+      matrix.pop();
+    }
+
+    return matrix;
+  }
+
+  private initLimitOption(cursor: Date, option: CalendarOption) {
+    const limitFirst: Date =
+      (option.limit && option.limit.first) ||
+      CalendarDate.createWebDateByLiteral({
+        year: 1970,
+        month: 1,
+        date: 1,
+      });
+    const limitLast: Date = (option.limit && option.limit.last) || cursor;
+    const clonedLimitFirst = new Date(limitFirst.getTime());
+    const clonedLimitLast = new Date(limitLast.getTime());
+
+    clonedLimitFirst.setHours(0);
+    clonedLimitLast.setHours(24);
+
+    this.limit = {
+      first: CalendarDate.toUnixTime(clonedLimitFirst),
+      last: CalendarDate.toUnixTime(clonedLimitLast),
+    };
+  }
+}
---
+++ util/Calendar/CalendarDate.ts
@@ -0,0 +1,221 @@
+export type UnixTime = number;
+
+export enum Day {
+  Sunday = 0,
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+}
+
+export enum Position {
+  prevMonth = -1,
+  currentMonth = 0,
+  nextMonth = 1,
+}
+
+type LiteralDate = {
+  year?: number;
+  month?: number;
+  date?: number;
+};
+
+type CalendarDateOption = {
+  cursor: Date;
+  limit: {
+    first: UnixTime;
+    last: UnixTime;
+  };
+};
+
+/**
+ * @see
+ * - 항상 12시로 설정됨 (최대, 최소날짜 비교를 편하게 하기 위함)
+ */
+export default class CalendarDate {
+  private webDate: Date;
+  private unix: UnixTime;
+  private cursor: Date;
+  private limit: {
+    first: UnixTime;
+    last: UnixTime;
+  };
+
+  constructor(webDate: Date, option: CalendarDateOption) {
+    this.webDate = webDate;
+    this.unix = CalendarDate.toUnixTime(webDate);
+    this.cursor = option.cursor;
+    this.limit = option.limit;
+  }
+
+  //////////////////////////////////////////
+  //////////// Getter/Setter ///////////////
+  //////////////////////////////////////////
+  public getFullDate(): [number, number, number] {
+    return [this.getYear(), this.getMonth(), this.getDate()];
+  }
+
+  public getYear(): number {
+    return this.webDate.getFullYear();
+  }
+
+  public getMonth(): number {
+    return this.webDate.getMonth() + 1;
+  }
+
+  public getDate(): number {
+    return this.webDate.getDate();
+  }
+
+  public getDay(): Day {
+    return this.webDate.getDay();
+  }
+
+  public getUnix(): UnixTime {
+    return CalendarDate.toUnixTime(this.webDate);
+  }
+
+  public getPosition(): Position {
+    const cursorYear = this.cursor.getFullYear();
+    const cursorMonth = this.cursor.getMonth() + 1;
+    const thisYear = this.getYear();
+    const thisMonth = this.getMonth();
+
+    if (cursorYear > thisYear) {
+      return Position.prevMonth;
+    } else if (cursorYear < thisYear) {
+      return Position.nextMonth;
+    }
+    if (cursorMonth > thisMonth) {
+      return Position.prevMonth;
+    } else if (cursorMonth < thisMonth) {
+      return Position.nextMonth;
+    }
+
+    return Position.currentMonth;
+  }
+
+  public checkActive(): boolean {
+    return this.unix < this.limit.last && this.unix >= this.limit.first;
+  }
+
+  public clone(): CalendarDate {
+    return new CalendarDate(new Date(this.webDate.getTime()), {
+      cursor: new Date(this.cursor.getTime()),
+      limit: { ...this.limit },
+    });
+  }
+
+  //////////////////////////////////////////
+  /////////////// Mutation /////////////////
+  //////////////////////////////////////////
+  public mutateToPrevDate(): void {
+    this.webDate = CalendarDate.createWebDateByLiteral({
+      year: this.getYear(),
+      month: this.getMonth(),
+      date: this.getDate() - 1,
+    });
+    this.unix = CalendarDate.toUnixTime(this.webDate);
+  }
+
+  public mutateToNextDate(): void {
+    this.webDate = CalendarDate.createWebDateByLiteral({
+      year: this.getYear(),
+      month: this.getMonth(),
+      date: this.getDate() + 1,
+    });
+    this.unix = CalendarDate.toUnixTime(this.webDate);
+  }
+
+  //////////////////////////////////////////
+  ///////////// Static Util ////////////////
+  //////////////////////////////////////////
+  public static createWebDateByLiteral({
+    year,
+    month,
+    date,
+  }: LiteralDate): Date {
+    const webDate = new Date();
+    const monthIndex = typeof month === 'number' ? month - 1 : month;
+
+    if (year !== undefined) webDate.setFullYear(year);
+    if (month !== undefined) webDate.setMonth(monthIndex);
+    if (date !== undefined) webDate.setDate(date);
+
+    webDate.setHours(12, 0, 0);
+
+    return webDate;
+  }
+
+  public static toUnixTime(webDate: Date): UnixTime {
+    return Math.floor(webDate.getTime() / 1000);
+  }
+
+  //////////////////////////////////////////
+  //////////////// Factory /////////////////
+  //////////////////////////////////////////
+  public static createByLiteral(
+    { year, month, date }: LiteralDate,
+    option: CalendarDateOption
+  ): CalendarDate {
+    return new CalendarDate(
+      CalendarDate.createWebDateByLiteral({
+        year,
+        month,
+        date,
+      }),
+      option
+    );
+  }
+
+  public static createFirstDate(
+    webDate: Date,
+    option: CalendarDateOption
+  ): CalendarDate {
+    const year = webDate.getFullYear();
+    const month = webDate.getMonth() + 1;
+
+    return CalendarDate.createByLiteral({ year, month, date: 1 }, option);
+  }
+
+  public static createLastDate(
+    webDate: Date,
+    option: CalendarDateOption
+  ): CalendarDate {
+    const year = webDate.getFullYear();
+    const month = webDate.getMonth() + 1;
+
+    return CalendarDate.createByLiteral(
+      { year, month: month + 1, date: 0 },
+      option
+    );
+  }
+
+  public static createFirstDateOfNextMonth(
+    webDate: Date,
+    option: CalendarDateOption
+  ): CalendarDate {
+    const year = webDate.getFullYear();
+    const month = webDate.getMonth() + 1;
+
+    return CalendarDate.createByLiteral(
+      { year, month: month + 1, date: 1 },
+      option
+    );
+  }
+
+  public static createLastDateOfPrevMonth(
+    webDate: Date,
+    option: CalendarDateOption
+  ): CalendarDate {
+    const year = webDate.getFullYear();
+    const month = webDate.getMonth() + 1;
+
+    return CalendarDate.createByLiteral(
+      { year, month: month, date: 0 },
+      option
+    );
+  }
+}
