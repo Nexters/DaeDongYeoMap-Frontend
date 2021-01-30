f8f74c76da152522688f5111c991233c9186a392
load0ne <jjazan201@gmail.com>
2021-01-30 11:31:04 +0900

    feat: apollo ssr

--- lib/apollo/client.ts
+++ lib/apollo/client.ts
@@ -1,8 +1,46 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import { useMemo } from 'react';
+import cache from './cache';
 
-const client = new ApolloClient({
-  uri: 'https://korean-date-map.herokuapp.com/graphql',
-  cache: new InMemoryCache(),
-});
+let apolloClient: ApolloClient<NormalizedCacheObject> = null;
 
-export default client;
+export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
+  const client = new ApolloClient({
+    ssrMode: !process.browser,
+    link: new HttpLink({
+      uri: 'https://korean-date-map.herokuapp.com/graphql',
+    }),
+    cache,
+  });
+
+  return client;
+};
+
+export const initializeApollo = (
+  initialState: NormalizedCacheObject = null
+): ApolloClient<NormalizedCacheObject> => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  if (initialState) {
+    const existingCache = _apolloClient.extract();
+
+    _apolloClient.cache.restore({ ...existingCache, ...initialState });
+  }
+
+  if (!process.browser) {
+    return _apolloClient;
+  }
+  if (!apolloClient) {
+    apolloClient = _apolloClient;
+  }
+
+  return _apolloClient;
+};
+
+export const useApollo = (
+  initialState?: NormalizedCacheObject
+): ApolloClient<NormalizedCacheObject> => {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+
+  return store;
+};
