74a92ed7dc6b54feeabe31f8e6c45abde7f5b333
load0ne <jjazan201@gmail.com>
2021-02-01 18:43:18 +0900

    feat: 스팟찍기 레이어 상태 설계

---
+++ components/Popup/SpotGenerator/SpotGeneratorState.ts
@@ -0,0 +1,45 @@
+import { makeVar, useReactiveVar } from '@apollo/client';
+
+type FormSugarState = string;
+type FormStickerState = string;
+type FormPartnerState = string;
+type FormDateState = string;
+type StateSetter<State> = (newState: State) => void;
+type UseStateHook<State> = () => [State, StateSetter<State>];
+
+const formSugarState = makeVar<FormSugarState>(null);
+const formStickerState = makeVar<FormStickerState>(null);
+const formPartnerState = makeVar<FormPartnerState>(null);
+const formDateState = makeVar<FormDateState>(null);
+
+export const useFormSugarState: UseStateHook<FormSugarState> = () => {
+  const formSugar: FormSugarState = useReactiveVar(formSugarState);
+  const setFormSugar: StateSetter<FormSugarState> = (newFormSugar) =>
+    formSugarState(newFormSugar);
+
+  return [formSugar, setFormSugar];
+};
+
+export const useFormStickerState: UseStateHook<FormStickerState> = () => {
+  const formSticker: FormStickerState = useReactiveVar(formStickerState);
+  const setFormSticker: StateSetter<FormStickerState> = (newFormSticker) =>
+    formStickerState(newFormSticker);
+
+  return [formSticker, setFormSticker];
+};
+
+export const useFormPartnerState: UseStateHook<FormPartnerState> = () => {
+  const formPartner: FormPartnerState = useReactiveVar(formPartnerState);
+  const setFormPartner: StateSetter<FormPartnerState> = (newFormPartner) =>
+    formPartnerState(newFormPartner);
+
+  return [formPartner, setFormPartner];
+};
+
+export const useFormDateState: UseStateHook<FormDateState> = () => {
+  const formDate: FormDateState = useReactiveVar(formDateState);
+  const setFormDate: StateSetter<FormDateState> = (newFormDate) =>
+    formDateState(newFormDate);
+
+  return [formDate, setFormDate];
+};
