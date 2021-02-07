028591f175225e9bb66fe600d3cff4b41088cb97
angrycatjenny <angrycatjenny@gmail.com>
2021-02-07 10:57:44 +0900

    feature/add search autocomplete

--- components/Home/SearchPlace/SearchPlace.tsx
+++ components/Home/SearchPlace/SearchPlace.tsx
@@ -1,33 +1,115 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import * as $ from './SearchPlaceView';
+import { gql, useLazyQuery } from '@apollo/client';
+import { debounce } from 'lodash';
+
+const PLACES_AND_SPOTS_BY_KEYWORDID = gql`
+  query placesAndSpotsByKeyword($query: String!, $keyword: String!) {
+    getPlacesByKeyword(filters: { query: $query }) {
+      id
+      place_name
+      category_name
+      category_group_name
+      address_name
+      road_address_name
+      x
+      y
+    }
+    getSpotsByKeyword(keyword: $keyword) {
+      _id
+      place_name
+      category_name
+      category_group_name
+      address_name
+      road_address_name
+      x
+      y
+    }
+  }
+`;
 
 const SearchPlace: React.FC = () => {
-  const onChangeInput = (e: any) => {
-    // TODO;
-  };
+  const [keyword, setKeyword] = useState('');
+  const query = keyword;
+  const [loadData, { called, loading, data }] = useLazyQuery(
+    PLACES_AND_SPOTS_BY_KEYWORDID,
+    {
+      variables: { query, keyword },
+      onError(error) {
+        console.log('error', error);
+      },
+    }
+  );
+  console.log('data', data);
+  console.log('type', typeof data);
+  if (data && data.getPlacesByKeyword) {
+    console.log('제발', data.getPlacesByKeyword);
+  }
+  if (data && data.getSpotsByKeyword) {
+    console.log('제발22', data.getSpotsByKeyword);
+  }
+
+  const debounceFunc = React.useCallback(debounce(loadData), [loadData]);
+
+  const onChangeInput = React.useCallback(
+    (e: any) => {
+      e.preventDefault();
+      setKeyword(e.target.value);
+      debounceFunc([loadData]);
+    },
+    [debounceFunc]
+  );
 
   const submitValue = (e: any) => {
     e.preventDefault();
-
-    // TODO;
   };
 
   return (
-    <$.SearchDiv>
-      <$.SearchForm onSubmit={submitValue}>
-        <$.TempImg />
-        <$.InputField
-          type="text"
-          name="searchValue"
-          placeholder={'장소를 검색하세요'}
-          value={''}
-          onChange={onChangeInput}
-        />
-      </$.SearchForm>
-      <$.SpotButton>
-        <$.SpotButtonImg />
-      </$.SpotButton>
-    </$.SearchDiv>
+    <$.ParentDiv>
+      <$.SearchDiv>
+        <$.SearchForm onSubmit={submitValue}>
+          <$.TempImg />
+          <$.InputField
+            type="text"
+            name="searchValue"
+            placeholder={'장소를 검색하세요'}
+            value={keyword}
+            onChange={onChangeInput}
+            autoFocus={true}
+            autoComplete="off"
+          />
+        </$.SearchForm>
+        <$.SpotButton>
+          <$.SpotButtonImg />
+        </$.SpotButton>
+      </$.SearchDiv>
+      {data && (
+        <$.PlacesAndSpots>
+          <$.Places>추천 스팟</$.Places>
+          {data &&
+            data.getPlacesByKeyword &&
+            data.getPlacesByKeyword
+              .slice(0, 2)
+              .map(({ id, place_name, address_name }) => (
+                <$.PlacesAndSpotsItem key={id}>
+                  {place_name}
+                  <$.AddressDiv>{address_name}</$.AddressDiv>
+                </$.PlacesAndSpotsItem>
+              ))}
+          <$.Spots>다른 사용자가 직접 등록했어요</$.Spots>
+          {data &&
+            data.getSpotsByKeyword &&
+            data.getSpotsByKeyword
+              .slice(0, 2)
+              .map(({ _id, place_name, address_name }) => (
+                <$.PlacesAndSpotsItem key={_id}>
+                  {place_name}
+                  <$.AddressDiv>{address_name}</$.AddressDiv>
+                </$.PlacesAndSpotsItem>
+              ))}
+        </$.PlacesAndSpots>
+      )}
+    </$.ParentDiv>
   );
 };
 
--- components/Home/SearchPlace/SearchPlaceView.tsx
+++ components/Home/SearchPlace/SearchPlaceView.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import painter from '~/styles/theme/painter';
 
+export const ParentDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  z-index: 3;
+`;
+
 export const SearchDiv = styled.div`
   position: absolute;
   top: 35px;
@@ -13,14 +19,14 @@ export const SearchDiv = styled.div`
 export const SearchForm = styled.form`
   display: flex;
   border: none;
-  border-radius: 6px;
+  border-radius: 6px 6px 0 0;
   outline: none;
   box-sizing: border-box;
-  background-color: #fff;
+  background-color: ${painter.basic.white};
 `;
 
 export const InputField = styled.input`
-  width: 270px;
+  width: 240px;
   height: 40px;
   font-size: 18px;
   padding: 10px;
@@ -34,10 +40,60 @@ export const InputField = styled.input`
   }
 `;
 
+export const PlacesAndSpots = styled.ul`
+  list-style: none;
+  z-index: 3;
+  border-top: 1px solid ${painter.grayscale['5']};
+  border-radius: 0 0 6px 6px;
+  background-color: ${painter.basic.white};
+  width: 292px;
+  position: absolute;
+  top: 82px;
+  left: 20px;
+  padding-bottom: 10px;
+`;
+
+export const Places = styled.div`
+  color: ${painter.grayscale['7']};
+  padding: 10px 5px 7px 5px;
+`;
+
+export const Spots = styled.div`
+  color: ${painter.grayscale['7']};
+  border-top: 1px solid ${painter.grayscale['5']};
+  padding: 10px 5px 7px 5px;
+  margin-top: 10px;
+`;
+
+export const PlacesAndSpotsItem = styled.li`
+  display: block;
+  font-size: 15px;
+  font-family: ${painter.font.roboto};
+  font-weight: bold;
+  padding: 7px 5px 7px 5px;
+  margin: 0;
+  text-decoration: none;
+  cursor: pointer;
+  color: ${painter.grayscale['9']};
+  .active {
+    border-bottom: solid 3px ${painter.grayscale['3']};
+  }
+  &:hover {
+    background-color: rgba(255, 159, 179, 0.25);
+  }
+`;
+
+export const AddressDiv = styled.div`
+  margin-top: 3px;
+  color: ${painter.grayscale['8']};
+  font-size: 13px;
+  font-family: ${painter.font.roboto};
+`;
+
 export const TempImg = styled.img.attrs({
   src: '/search.png',
 })`
-  width: 32px;
+  width: 30px;
   height: 32px;
   margin: 8px 7px 0 15px;
 `;
