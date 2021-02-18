9a811c76c20956d65da0cd4cd02117b2ee576750
load0ne <jjazan201@gmail.com>
2021-02-18 20:20:45 +0900

    feat: 타임스탬프 포맷팅 적용

--- util/index.ts
+++ util/index.ts
@@ -16,12 +16,19 @@ export const getPathSegment = (pathname: string, index: number): string => {
   return pathname.split('/').filter((path) => !!path)[index] || '';
 };
 
-export const formatDate = (unixTime: number): string => {
-  const dateObject: Date = new Date(unixTime);
+export const formatDate = (
+  unixTime: number,
+  isKoreanType?: boolean
+): string => {
+  const dateObject: Date = new Date(unixTime * 1000);
 
   const year: number = dateObject.getFullYear();
   const month: string = toDigit(dateObject.getMonth() + 1, 2);
   const date: string = toDigit(dateObject.getDate(), 2);
+
+  if (isKoreanType) {
+    return `${year}.${month}.${date}`;
+  }
 
   return `${date}.${month}.${year}`;
 };
